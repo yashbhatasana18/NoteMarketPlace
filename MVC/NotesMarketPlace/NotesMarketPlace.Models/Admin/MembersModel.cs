e02d45a6c4b84bf808313724fcfe3f8ed1e0bb60
YASH BHATASANA <51210033+yashbhatasana18@users.noreply.github.com>
2021-04-09 23:26:01 +0530

    Assignment-3

---
+++ MVC/NotesMarketPlace/NotesMarketPlace.Models/Admin/MembersModel.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotesMarketPlace.Models.Admin
+{
+    public class MembersModel
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string ProfileImage { get; set; }
+
+        public string Email { get; set; }
+
+        public DateTime? JoinDate { get; set; }
+
+        public int UnderReviewNotes { get; set; }
+
+        public int PublishedNotes { get; set; }
+
+        public int DownloadedNotes { get; set; }
+
+        public decimal? TotalExpense { get; set; }
+
+        public decimal? TotalEarning { get; set; }
+
+        // member details
+
+        public DateTime? DOB { get; set; }
+
+        public string Phone { get; set; }
+
+        public string University { get; set; }
+
+        public string Address1 { get; set; }
+
+        public string Address2 { get; set; }
+
+        public string City { get; set; }
+
+        public string State { get; set; }
+
+        public string Country { get; set; }
+
+        public string Zipcode { get; set; }
+    }
+
+    public class MemberNoteModel
+    {
+        public int Id { get; set; }
+
+        public int NoteId { get; set; }
+
+        public string Title { get; set; }
+
+        public string Category { get; set; }
+
+        public string Status { get; set; }
+
+        public int DownloadedNote { get; set; }
+
+        public decimal? Earning { get; set; }
+
+        public DateTime? DateAdded { get; set; }
+
+        public DateTime? PublishedDate { get; set; }
+    }
+}
--- MVC/NotesMarketPlace/NotesMarketPlace/Controllers/AdminController.cs
+++ MVC/NotesMarketPlace/NotesMarketPlace/Controllers/AdminController.cs
@@ -74,7 +74,7 @@ namespace NotesMarketPlace.Controllers
 
         #region Dashboard
 
-        public ActionResult Dashboard(int? month)
+        public ActionResult Dashboard(int? month, string txtSearch, string SortOrder, string SortBy, int PageNumber = 1)
         {
             //if (Session["emailID"] == null)
             //{
@@ -128,28 +128,387 @@ namespace NotesMarketPlace.Controllers
                                 filename = Attachment.FilePath
                             }).OrderByDescending(x => x.TotalDownloads).ToList();
 
+                ViewBag.SortOrder = SortOrder;
+                ViewBag.SortBy = SortBy;
+                var Dashboardresult = note;
+
+                if (txtSearch != null)
+                {
+                    Dashboardresult = Dashboardresult.Where(x => x.Title.ToLower().Contains(txtSearch.ToLower())).ToList();
+
+                    //Sorting
+                    Dashboardresult = ApplySorting(SortOrder, SortBy, Dashboardresult);
+
+                    //Pagination
+                    Dashboardresult = ApplyPagination(Dashboardresult, PageNumber);
+                }
+                else
+                {
+                    //Sorting
+                    Dashboardresult = ApplySorting(SortOrder, SortBy, Dashboardresult);
+
+                    //Pagination
+                    Dashboardresult = ApplyPagination(Dashboardresult, PageNumber);
+                }
+
                 // append attachment size
-                foreach (var data in note)
+                foreach (var data in Dashboardresult)
                 {
                     data.AttachmentSize = GetSize(data.userid, data.Id, data.filename);
                 }
 
                 if (month == null)
                 {
-                    var filternote = note.Where(m => m.publishMonth == DateTime.Now.Month).ToList();
+                    var filternote = Dashboardresult.Where(m => m.publishMonth == DateTime.Now.Month).ToList();
                     return View(filternote);
                 }
                 else
                 {
-                    var filternote = note.Where(m => m.publishMonth == month).ToList();
+                    var filternote = Dashboardresult.Where(m => m.publishMonth == month).ToList();
                     return View(filternote);
                 }
+            }
+        }
 
+        #endregion Dashboard
+
+        #region Members
+
+        public ActionResult Members(string txtSearch, string SortOrder, string SortBy, int PageNumber = 1)
+        {
+            using (var context = new NotesMarketPlaceEntities())
+            {
+
+                var model = (from User in context.Users
+                             where User.RoleID == 3 && User.IsActive == true
+                             let underReview = (from Notes in context.SellerNotes
+                                                where Notes.SellerID == User.UserID && (Notes.Status == 7 || Notes.Status == 8)
+                                                select Notes).Count()
+                             let published = (from Notes in context.SellerNotes
+                                              where Notes.SellerID == User.UserID && Notes.Status == 9
+                                              select Notes).Count()
+                             let downloaded = (from Purchase in context.Downloads
+                                               where Purchase.Downloader == User.UserID && Purchase.IsAttachmentDownloaded == true
+                                               select Purchase)
+                             let sell = (from Purchase in context.Downloads
+                                         where Purchase.Seller == User.UserID && Purchase.IsSellerHasAllowedDownload == true
+                                         select Purchase)
+                             select new MembersModel
+                             {
+                                 Id = User.UserID,
+                                 FirstName = User.FirstName,
+                                 LastName = User.LastName,
+                                 Email = User.EmailID,
+                                 JoinDate = User.CreatedDate,
+                                 UnderReviewNotes = underReview,
+                                 PublishedNotes = published,
+                                 DownloadedNotes = downloaded.Count(),
+                                 TotalExpense = downloaded.Sum(x => x.PurchasedPrice),
+                                 TotalEarning = sell.Sum(x => x.PurchasedPrice)
+                             }).OrderByDescending(x => x.JoinDate).ToList();
+
+                ViewBag.SortOrder = SortOrder;
+                ViewBag.SortBy = SortBy;
+                var Membersresult = model;
+
+                if (txtSearch != null)
+                {
+                    Membersresult = Membersresult.Where(x => x.FirstName.ToLower().Contains(txtSearch.ToLower())).ToList();
+                    Membersresult = Membersresult.Where(x => x.LastName.ToLower().Contains(txtSearch.ToLower())).ToList();
+                    Membersresult = Membersresult.Where(x => x.Email.ToLower().Contains(txtSearch.ToLower())).ToList();
+
+                    //Sorting
+                    Membersresult = ApplySorting(SortOrder, SortBy, Membersresult);
+
+                    //Pagination
+                    Membersresult = ApplyPagination(Membersresult, PageNumber);
+                }
+                else
+                {
+                    //Sorting
+                    Membersresult = ApplySorting(SortOrder, SortBy, Membersresult);
+
+                    //Pagination
+                    Membersresult = ApplyPagination(Membersresult, PageNumber);
+                }
+
+                return View(model);
             }
 
         }
 
-        #endregion Dashboard
+        #endregion Members
+
+        #region Member Details
+
+        public ActionResult MemberDetails(int id, string SortOrder, string SortBy, int PageNumber = 1)
+        {
+            using (var context = new NotesMarketPlaceEntities())
+            {
+                // default img
+                var DefaultImg = context.SystemConfigurations.SingleOrDefault(m => m.Key == "DefaultProfileImage").Value;
+
+                // member details
+                var details = (from User in context.Users
+                               where User.UserID == id
+                               join Details in context.UserProfile on User.UserID equals Details.UserID
+                               join Country in context.Countries on Details.Country equals Country.Name
+                               select new MembersModel
+                               {
+                                   FirstName = User.FirstName,
+                                   LastName = User.LastName,
+                                   ProfileImage = Details.ProfilePicture == null ? DefaultImg.Remove(0, 2) : Details.ProfilePicture.Remove(0, 2),
+                                   Email = User.EmailID,
+                                   DOB = Details.DOB,
+                                   Phone = Details.PhoneNumber,
+                                   University = Details.University,
+                                   Address1 = Details.AddressLine1,
+                                   Address2 = Details.AddressLine2,
+                                   City = Details.City,
+                                   State = Details.State,
+                                   Country = Country.Name,
+                                   Zipcode = Details.ZipCode
+                               }).SingleOrDefault();
+
+                ViewBag.Details = details;
+
+                // member notes
+                var notes = (from Note in context.SellerNotes
+                             where Note.SellerID == id
+                             join Status in context.ReferenceData on Note.Status equals Status.ReferenceDataID
+                             join Category in context.NoteCategories on Note.Category equals Category.NoteCategoriesID
+                             let downloadedNotes = (context.Downloads.Where(m => m.Downloader == id && m.IsAttachmentDownloaded == true).Count())
+                             let earning = (context.Downloads.Where(m => m.Seller == id).Sum(x => x.PurchasedPrice))
+                             select new MemberNoteModel
+                             {
+                                 NoteId = Note.SellerNotesID,
+                                 Title = Note.Title,
+                                 Category = Category.Name,
+                                 Status = Status.Value,
+                                 DownloadedNote = downloadedNotes,
+                                 Earning = earning,
+                                 DateAdded = Note.CreatedDate,
+                                 PublishedDate = Note.PublishedDate
+                             }).ToList();
+
+                ViewBag.SortOrder = SortOrder;
+                ViewBag.SortBy = SortBy;
+                var MemberDetailsNoteresult = notes;
+
+                //Sorting
+                MemberDetailsNoteresult = ApplySorting(SortOrder, SortBy, MemberDetailsNoteresult);
+
+                //Pagination
+                MemberDetailsNoteresult = ApplyPagination(MemberDetailsNoteresult, PageNumber);
+
+                return View(MemberDetailsNoteresult);
+            }
+        }
+
+        #endregion Members Details
+
+        #region Apply Sorting
+
+        public List<DashboardModel> ApplySorting(string SortOrder, string SortBy, List<DashboardModel> result)
+        {
+            switch (SortBy)
+            {
+                case "Category":
+                    {
+                        switch (SortOrder)
+                        {
+                            case "Asc":
+                                {
+                                    result = result.OrderBy(x => x.Category).ToList();
+                                    break;
+                                }
+                            case "Desc":
+                                {
+                                    result = result.OrderByDescending(x => x.Category).ToList();
+                                    break;
+                                }
+                            default:
+                                {
+                                    result = result.OrderBy(x => x.Category).ToList();
+                                    break;
+                                }
+                        }
+                        break;
+                    }
+                case "Title":
+                    {
+                        switch (SortOrder)
+                        {
+                            case "Asc":
+                                {
+                                    result = result.OrderBy(x => x.Title).ToList();
+                                    break;
+                                }
+                            case "Desc":
+                                {
+                                    result = result.OrderByDescending(x => x.Title).ToList();
+                                    break;
+                                }
+                            default:
+                                {
+                                    result = result.OrderBy(x => x.Title).ToList();
+                                    break;
+                                }
+                        }
+                        break;
+                    }
+                default:
+                    result = result.OrderByDescending(x => x.PublishDate).ToList();
+                    break;
+            }
+            return result;
+        }
+
+        public List<MembersModel> ApplySorting(string SortOrder, string SortBy, List<MembersModel> result)
+        {
+            switch (SortBy)
+            {
+                case "Category":
+                    {
+                        switch (SortOrder)
+                        {
+                            case "Asc":
+                                {
+                                    result = result.OrderBy(x => x.FirstName).ToList();
+                                    break;
+                                }
+                            case "Desc":
+                                {
+                                    result = result.OrderByDescending(x => x.FirstName).ToList();
+                                    break;
+                                }
+                            default:
+                                {
+                                    result = result.OrderBy(x => x.FirstName).ToList();
+                                    break;
+                                }
+                        }
+                        break;
+                    }
+                case "Title":
+                    {
+                        switch (SortOrder)
+                        {
+                            case "Asc":
+                                {
+                                    result = result.OrderBy(x => x.LastName).ToList();
+                                    break;
+                                }
+                            case "Desc":
+                                {
+                                    result = result.OrderByDescending(x => x.LastName).ToList();
+                                    break;
+                                }
+                            default:
+                                {
+                                    result = result.OrderBy(x => x.LastName).ToList();
+                                    break;
+                                }
+                        }
+                        break;
+                    }
+                default:
+                    result = result.OrderByDescending(x => x.JoinDate).ToList();
+                    break;
+            }
+            return result;
+        }
+
+        public List<MemberNoteModel> ApplySorting(string SortOrder, string SortBy, List<MemberNoteModel> result)
+        {
+            switch (SortBy)
+            {
+                case "Category":
+                    {
+                        switch (SortOrder)
+                        {
+                            case "Asc":
+                                {
+                                    result = result.OrderBy(x => x.Title).ToList();
+                                    break;
+                                }
+                            case "Desc":
+                                {
+                                    result = result.OrderByDescending(x => x.Title).ToList();
+                                    break;
+                                }
+                            default:
+                                {
+                                    result = result.OrderBy(x => x.Title).ToList();
+                                    break;
+                                }
+                        }
+                        break;
+                    }
+                case "Title":
+                    {
+                        switch (SortOrder)
+                        {
+                            case "Asc":
+                                {
+                                    result = result.OrderBy(x => x.Category).ToList();
+                                    break;
+                                }
+                            case "Desc":
+                                {
+                                    result = result.OrderByDescending(x => x.Category).ToList();
+                                    break;
+                                }
+                            default:
+                                {
+                                    result = result.OrderBy(x => x.Category).ToList();
+                                    break;
+                                }
+                        }
+                        break;
+                    }
+                default:
+                    result = result.OrderByDescending(x => x.DateAdded).ToList();
+                    break;
+            }
+            return result;
+        }
+
+        #endregion Apply Sorting
+
+        #region Apply Pagination
+
+        public List<DashboardModel> ApplyPagination(List<DashboardModel> result, int PageNumber)
+        {
+            ViewBag.TotalPages = Math.Ceiling(result.Count() / 5.0);
+            ViewBag.PageNumber = PageNumber;
+
+            result = result.Skip((PageNumber - 1) * 5).Take(5).ToList();
+
+            return result;
+        }
+
+        public List<MembersModel> ApplyPagination(List<MembersModel> result, int PageNumber)
+        {
+            ViewBag.TotalPages = Math.Ceiling(result.Count() / 5.0);
+            ViewBag.PageNumber = PageNumber;
+
+            result = result.Skip((PageNumber - 1) * 5).Take(5).ToList();
+
+            return result;
+        }
+
+        public List<MemberNoteModel> ApplyPagination(List<MemberNoteModel> result, int PageNumber)
+        {
+            ViewBag.TotalPages = Math.Ceiling(result.Count() / 5.0);
+            ViewBag.PageNumber = PageNumber;
+
+            result = result.Skip((PageNumber - 1) * 5).Take(5).ToList();
+
+            return result;
+        }
+
+        #endregion Apply Pagination
 
         // returns file size in KB
         public float GetSize(int user, int note, string filename)
@@ -161,13 +520,13 @@ namespace NotesMarketPlace.Controllers
 
         public ActionResult LogOut()
         {
-            if (Session["emailID"] != null)
-            {
-                Session.Abandon();
-                return RedirectToAction("Login", "Account");
-            }
-            //FormsAuthentication.SignOut();
-            return View();
+            //if (Session["emailID"] != null)
+            //{
+            //    Session.Abandon();
+            //    return RedirectToAction("Login", "Account");
+            //}
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login", "Account");
         }
     }
 }
